1abbda39fc154fa46567a22fffba63a3109a6cc4
bakerv <80018640+bakerv@users.noreply.github.com>
2021-07-24 17:08:35 -0500

    successfully import geojson data to the page, creating leaflat layers using mapbox

--- leaflet-step-1/main.js
+++ leaflet-step-1/main.js
@@ -1,1 +1,30 @@
 let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
+var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
+"2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337"
+
+
+d3.json(url).then(data => {
+    console.log(data.features);
+});
+
+function generateMap(earthquakes) {
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+    });
+
+    var overlapMaps = {}
+    var myMap = L.map("map", {
+        center: [37.09,-95.71],
+        zoom: 5,
+        layers: [streetmap]
+    });
+
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false }).addTo(myMap);
+
+}
