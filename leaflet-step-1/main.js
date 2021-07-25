7d4582529fdcae81225a6acee48fef50f61ef884
bakerv <80018640+bakerv@users.noreply.github.com>
2021-07-25 18:14:07 -0500

    successfully added legend, css styling needs improvement

--- leaflet-step-1/main.js
+++ leaflet-step-1/main.js
@@ -5,36 +5,26 @@ d3.json(url).then(data => {
 
     var earthquakes = L.geoJSON(data.features, {
         pointToLayer: function(feature, latlng) {
-            // dynamically color the markers, based on the depth of the earthquake
-            var colorScale = ""
-            if (feature.geometry.coordinates[2] < 10){
-                colorScale = "green";
-            }
-            else if (feature.geometry.coordinates[2] < 20){
-                colorScale = "blue";
-            }
-            else if (feature.geometry.coordinates[2] < 30){
-                colorScale = "yellow";
-            }
-            else if (feature.geometry.coordinates[2] < 40){
-                colorScale = "orange";
-            }
-            else {
-                colorScale = "red"
-            }
+       
             // dynamic attributes to pass to L.circleMarker() 
             var markerAttributes = {
-                radius: 10 + (5 ** feature.properties.mag /1000) ,
-                color: colorScale,
-                weight: 0,
+                radius: 10 + (5 ** feature.properties.mag /1000),
+                color: 'black',
+                fillColor: setColor(feature.geometry.coordinates[2]),
+                weight: 1,
                 fillOpacity: 0.8
-            }
+            };
+            var date = new Date(feature.properties.time).toString();
             // for each feature, create a circular marker, and bind a popup to that marker
             return L.circleMarker(latlng,markerAttributes)
-                .bindPopup("<strong>" +feature.properties.title +"</strong>" + "<br>" +Date(feature.properties.time));
+                .bindPopup("<strong>" +feature.properties.title + "</strong>" + "<br>" 
+                + date + "<br><br>"
+                + "Latitude:" + feature.geometry.coordinates[0] + "<br>"
+                + "Longitude: "+ feature.geometry.coordinates[1] + "<br>"
+                +"Depth: "+ feature.geometry.coordinates[2] + " km");
         }
+        
     });
-
     generateMap(earthquakes)
 });
 
@@ -58,15 +48,63 @@ function generateMap(earthquakes) {
     var myMap = L.map("map", {
         center: [37.09,-95.71],
         zoom: 5,
-        layers: [streetmap]
+        layers: [streetmap],
+        worldCopyJump: "True"
     });
 
     L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false }).addTo(myMap);
+        collapsed: false 
+    }).addTo(myMap);
+
+    var legend = L.control({position:"bottomleft"});
+
+    legend.onAdd = function(myMap) {
+        var div = L.DomUtil.create("div", "legend");
+        div.innerHTML += '<h3>Depth (km)</h3>';
+        div.innerHTML += '<i style="background: #ffffcc"></i><span>0-10</span><br>';
+        div.innerHTML += '<i style="background: #ffeda0"></i><span>11-20</span><br>';
+        div.innerHTML += '<i style="background: #fed976"></i><span>21-30</span><br>';
+        div.innerHTML += '<i style="background: #feb24c"></i><span>31-40</span><br>';
+        div.innerHTML += '<i style="background: #fd8d3c"></i><span>41-50</span><br>';
+        div.innerHTML += '<i style="background: #fc4e2a"></i><span>51-60</span><br>';
+        div.innerHTML += '<i style="background: #e31a1c"></i><span>61-70</span><br>';
+        div.innerHTML += '<i style="background: #b10026"></i><span> >70</span><br>';
+
+
+        return div;
     }
 
-function bindFeatures(feature,layer) {
-    layer.bindPopup(feature.properties.title);
+    legend.addTo(myMap);
+};
+
+function setColor(data) {
+    var colorScale = ""
+    if (data <= 10){
+        colorScale = "#ffffcc";
+    }
+    else if (data <= 20){
+        colorScale = "#ffeda0";
+    }
+    else if (data <= 30){
+        colorScale = "#fed976";
+    }
+    else if (data <= 40){
+        colorScale = "#feb24c";
+    }
+    else if (data <= 50){
+        colorScale = "#fd8d3c";
+    }
+    else if (data <= 60){
+        colorScale = "#fc4e2a";
+    }
+    else if (data <= 70){
+        colorScale = "#e31a1c";
+    }
+    else {
+        colorScale = "#b10026"
+    }
+
+    return colorScale;
 }
 
 
