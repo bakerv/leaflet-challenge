3e9d747abc0a6b200e8887a70407b51ad95b495c
bakerv <80018640+bakerv@users.noreply.github.com>
2021-07-26 16:33:22 -0500

    added additional base maps

--- leaflet-step-2/main.js
+++ leaflet-step-2/main.js
@@ -16,18 +16,37 @@ var map = L.map("map", {
     ]
 });
 
-var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
 attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
 tileSize: 512,
 maxZoom: 18,
 zoomOffset: -1,
 id: "mapbox/streets-v11",
 accessToken: API_KEY
+});
+
+var darkMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+tileSize: 512,
+maxZoom: 18,
+zoomOffset: -1,
+id: "mapbox/dark-v10",
+accessToken: API_KEY
 }).addTo(map);
 
+var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+tileSize: 512,
+maxZoom: 18,
+zoomOffset: -1,
+id: "mapbox/satellite-v9",
+accessToken: API_KEY
+});
 
-var baseMaps = {
-    "Street Map": streetmap
+ baseMaps = {
+    "Dark Map": darkMap,
+    "Satellite": satelliteMap,
+    "Street Map": streetMap
 };
 
 L.control.layers(baseMaps, overlayMaps, {
